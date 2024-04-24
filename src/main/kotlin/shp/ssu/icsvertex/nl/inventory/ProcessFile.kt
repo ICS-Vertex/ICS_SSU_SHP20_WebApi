ac79d04dc627bb560ef323d0b4a3d1942d13de7e
M.vanDijk <m.vandijk@ams.icsvertex.nl>
2024-04-24 16:54:23 +0200

    Changes by m. van dijk Routing updated

---
+++ src/main/kotlin/shp/ssu/icsvertex/nl/inventory/ProcessFile.kt
@@ -0,0 +1,38 @@
+package shp.ssu.icsvertex.nl.inventory
+
+import com.google.gson.Gson
+import io.ktor.http.content.*
+import io.ktor.server.plugins.*
+import nl.icsvertex.ssu.shp.core.models.inventory.InventoryOrder
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+
+
+suspend fun processFiles(input: MultiPartData): Boolean {
+    input.forEachPart { file ->
+        when (file) {
+            is PartData.FileItem -> {
+                if (file.originalFileName!!.lowercase().contains(".zip")) {
+
+                    val zipFile = ZipInputStream(file.streamProvider())
+
+                    val inventoryOrder = buildList<InventoryOrder> {
+                        var zipEntry: ZipEntry? = zipFile.nextEntry
+                        while (zipEntry != null) {
+                            if (!zipEntry.isDirectory) {
+                                zipFile.bufferedReader(charset("UTF-8")).readText().let {
+                                    add(Gson().fromJson(it, InventoryOrder::class.java))
+                                }
+                            }
+                            zipEntry = zipFile.nextEntry
+                        }
+                    }
+                    inventoryOrder.forEach { it.processInput() }
+                }
+            }
+
+            else -> throw BadRequestException("Import failed")
+        }
+    }
+    return true
+}
--- src/main/kotlin/shp/ssu/icsvertex/nl/plugins/Routing.kt
+++ src/main/kotlin/shp/ssu/icsvertex/nl/plugins/Routing.kt
@@ -1,79 +1,45 @@
 package shp.ssu.icsvertex.nl.plugins
 
-import com.google.gson.Gson
-import io.github.smiley4.ktorswaggerui.SwaggerUI
 import io.github.smiley4.ktorswaggerui.dsl.post
 import io.ktor.http.*
-import io.ktor.http.content.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.request.*
-import io.ktor.server.resources.Resources
+import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.coroutines.*
-import nl.icsvertex.ssu.shp.core.models.inventory.InventoryOrder
-import shp.ssu.icsvertex.nl.inventory.processInput
+import shp.ssu.icsvertex.nl.inventory.processFiles
 import java.io.File
-import java.util.zip.ZipEntry
-
-import java.util.zip.ZipInputStream
 
 fun Application.configureRouting() {
     install(StatusPages) {
         exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
+            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
         }
     }
     install(Resources)
     routing {
 //        route("\"${System.getenv("APPPATH")}" ) {
-            post("importInventory", {
-                request {
-                    multipartBody {
-                        mediaType(ContentType.MultiPart.FormData)
-                        required = true
-                        part<File>("zipfile") {
-                            mediaTypes = setOf(
-                                ContentType.Application.Zip
-                            )
-                        }
-                    }
-                }
-                response {
-                    HttpStatusCode.OK
-                }
-            }) {
-                val multiPartData = call.receiveMultipart()
-                multiPartData.forEachPart { file ->
-                    when (file) {
-                        is PartData.FileItem -> {
-                            if (file.originalFileName!!.lowercase().contains(".zip")) {
-
-                                val zipFile = ZipInputStream(file.streamProvider())
-
-                                val inventoryOrder = buildList<InventoryOrder> {
-                                    var zipEntry: ZipEntry? = zipFile.nextEntry
-                                    while (zipEntry != null){
-                                        if(!zipEntry.isDirectory){
-                                            zipFile.bufferedReader(charset("UTF-8")).readText().let {
-                                                add(Gson().fromJson(it, InventoryOrder::class.java))
-                                            }
-                                        }
-                                        zipEntry = zipFile.nextEntry
-                                    }
-                                }
-
-                                inventoryOrder.forEach { it.processInput() }
-
-                            }
-                        }
-
-                        else -> throw BadRequestException("Import failed")
+        post("importInventory", {
+            request {
+                multipartBody {
+                    mediaType(ContentType.MultiPart.FormData)
+                    required = true
+                    part<File>("zipfile") {
+                        mediaTypes = setOf(
+                            ContentType.Application.Zip
+                        )
                     }
                 }
             }
+            response {
+                HttpStatusCode.OK
+            }
+        }) {
+            val multiPartData = call.receiveMultipart()
+            processFiles(multiPartData)
+            call.respondText(text = "200", status = HttpStatusCode.OK)
         }
+    }
 //    }
 }
